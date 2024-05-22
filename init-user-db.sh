1d8025534ed7591bfad35bbfe54fd6bfab230abb
goban <everynothing@gmail.com>
2024-05-21 20:15:12 -0600

    more

--- init-user-db.sh
+++ init-user-db.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-echo "$POSTGRES_USER $POSTGRES_DB"
+echo "zzzzzzzzzzzzzz $POSTGRES_USER qqqqq $POSTGRES_PASSWORD yyyyyyyy $POSTGRES_DB"
 
 echo "host replication all 0.0.0.0/0 md5" >> /var/lib/postgresql/data/pg_hba.conf
 echo "wal_level = logical" >> /var/lib/postgresql/data/postgresql.conf

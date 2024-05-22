8928155a607b1c693d2364819b438cd3546a4354
goban <everynothing@gmail.com>
2024-05-21 22:44:59 -0600

    workin

--- init-user-db.sh
+++ init-user-db.sh
@@ -6,7 +6,6 @@ echo "zzzzzzzzzzzzzz $POSTGRES_USER qqqqq $POSTGRES_PASSWORD yyyyyyyy $POSTGRES_
 echo "host replication all 0.0.0.0/0 md5" >> /var/lib/postgresql/data/pg_hba.conf
 echo "wal_level = logical" >> /var/lib/postgresql/data/postgresql.conf
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-	CREATE DATABASE postgres-predictive;
-	GRANT ALL PRIVILEGES ON DATABASE postgres-predictive TO postgres;
-EOSQL
+# psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" << EOSQL
+# 	GRANT ALL PRIVILEGES ON DATABASE "$POSTGRES_DB" TO "$POSTGRES_USER";
+# EOSQL
